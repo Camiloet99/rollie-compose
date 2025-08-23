2c786d8be333aebed6856a798f221f5d7ed1a479
Camilo Echeverri <camiloet98@gmail.com>
2025-08-23 11:35:19 -0500

    bump

--- frontend/src/components/search/SearchForm.jsx
+++ frontend/src/components/search/SearchForm.jsx
@@ -1,4 +1,3 @@
-// src/components/search/SearchForm.jsx
 import React from "react";
 import { Button, Form, Row, Col, Spinner } from "react-bootstrap";
 import { FaSearch, FaTimesCircle } from "react-icons/fa";
@@ -9,7 +8,7 @@ export default function SearchForm({
   setFilters,
   onSubmit,
   loading = false,
-  showAdvancedEnabled = false,
+  showAdvancedEnabled = true,
   showAdvanced = false,
   setShowAdvanced = () => {},
   autocompleteEnabled = false,
@@ -40,9 +39,10 @@ export default function SearchForm({
 
   return (
     <Form onKeyDown={handleKeyDown}>
+      {/* ===== BASIC: SOLO Reference + Data from ===== */}
       <Row className="g-3 align-items-end">
-        {/* Reference */}
-        <Col md={4}>
+        {/* Reference Number */}
+        <Col md={6} lg={6}>
           <Form.Group controlId="reference">
             <Form.Label>Reference Number</Form.Label>
             <div className="position-relative">
@@ -55,8 +55,6 @@ export default function SearchForm({
                 autoComplete="off"
                 disabled={loading}
               />
-
-              {/* Clear reference */}
               {filters.reference && (
                 <FaTimesCircle
                   role="button"
@@ -65,8 +63,6 @@ export default function SearchForm({
                   title="Clear"
                 />
               )}
-
-              {/* Autocomplete con ReferenceSuggestions */}
               {autocompleteEnabled && (
                 <ReferenceSuggestions
                   open={suggestionsOpen}
@@ -81,8 +77,8 @@ export default function SearchForm({
           </Form.Group>
         </Col>
 
-        {/* Ventana temporal */}
-        <Col md={3}>
+        {/* Data from (window) */}
+        <Col md={4} lg={4}>
           <Form.Group controlId="window">
             <Form.Label>Data from</Form.Label>
             <Form.Select
@@ -92,42 +88,8 @@ export default function SearchForm({
               disabled={loading}
             >
               <option value="today">Today</option>
-              <option value="7d">Last 7 days (avg.)</option>
-              <option value="15d">Last 15 days (avg.)</option>
-            </Form.Select>
-          </Form.Group>
-        </Col>
-
-        {/* Color */}
-        <Col md={3}>
-          <Form.Group controlId="color">
-            <Form.Label>Color</Form.Label>
-            <Form.Control
-              name="color"
-              value={filters.color || ""}
-              onChange={handleChange}
-              placeholder="Dial color"
-              disabled={loading}
-            />
-          </Form.Group>
-        </Col>
-
-        {/* Condition */}
-        <Col md={2}>
-          <Form.Group controlId="condition">
-            <Form.Label>Condition</Form.Label>
-            <Form.Select
-              name="condition"
-              value={filters.condition || ""}
-              onChange={handleChange}
-              disabled={loading}
-            >
-              <option value="">Any</option>
-              <option value="new">New</option>
-              <option value="like_new">Like New</option>
-              <option value="very_good">Very Good</option>
-              <option value="good">Good</option>
-              <option value="fair">Fair</option>
+              <option value="7d">Last 7 days (avg)</option>
+              <option value="15d">Last 15 days (avg)</option>
             </Form.Select>
           </Form.Group>
         </Col>
@@ -141,7 +103,7 @@ export default function SearchForm({
               onClick={() => setShowAdvanced((v) => !v)}
               disabled={loading}
             >
-              {showAdvanced ? "Ocultar avanzados" : "Ver avanzados"}
+              {showAdvanced ? "Hide advanced" : "Show advanced"}
             </Button>
           )}
           <Button
@@ -164,9 +126,44 @@ export default function SearchForm({
         </Col>
       </Row>
 
-      {/* Filtros avanzados */}
+      {/* ===== ADVANCED ===== */}
       {showAdvanced && (
         <Row className="g-3 mt-1">
+          {/* Color */}
+          <Col md={3}>
+            <Form.Group controlId="color">
+              <Form.Label>Color</Form.Label>
+              <Form.Control
+                name="color"
+                value={filters.color || ""}
+                onChange={handleChange}
+                placeholder="Dial color"
+                disabled={loading}
+              />
+            </Form.Group>
+          </Col>
+
+          {/* Condition */}
+          <Col md={3}>
+            <Form.Group controlId="condition">
+              <Form.Label>Condition</Form.Label>
+              <Form.Select
+                name="condition"
+                value={filters.condition || ""}
+                onChange={handleChange}
+                disabled={loading}
+              >
+                <option value="">Any</option>
+                <option value="new">New</option>
+                <option value="like_new">Like New</option>
+                <option value="very_good">Very Good</option>
+                <option value="good">Good</option>
+                <option value="fair">Fair</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+
+          {/* Material */}
           <Col md={3}>
             <Form.Group controlId="material">
               <Form.Label>Material</Form.Label>
@@ -180,6 +177,7 @@ export default function SearchForm({
             </Form.Group>
           </Col>
 
+          {/* Production Year */}
           <Col md={3}>
             <Form.Group controlId="year">
               <Form.Label>Production Year</Form.Label>
@@ -193,6 +191,7 @@ export default function SearchForm({
             </Form.Group>
           </Col>
 
+          {/* Min / Max Price */}
           <Col md={3}>
             <Form.Group controlId="priceMin">
               <Form.Label>Min Price</Form.Label>
@@ -207,7 +206,6 @@ export default function SearchForm({
               />
             </Form.Group>
           </Col>
-
           <Col md={3}>
             <Form.Group controlId="priceMax">
               <Form.Label>Max Price</Form.Label>
@@ -223,6 +221,7 @@ export default function SearchForm({
             </Form.Group>
           </Col>
 
+          {/* Currency */}
           <Col md={3}>
             <Form.Group controlId="currency">
               <Form.Label>Currency</Form.Label>
@@ -241,6 +240,7 @@ export default function SearchForm({
             </Form.Group>
           </Col>
 
+          {/* Extra Info */}
           <Col md={6}>
             <Form.Group controlId="extraInfo">
               <Form.Label>Extra Info</Form.Label>
@@ -254,6 +254,7 @@ export default function SearchForm({
             </Form.Group>
           </Col>
 
+          {/* Brand */}
           <Col md={3}>
             <Form.Group controlId="brand">
               <Form.Label>Brand</Form.Label>
